e3476a148f75e5fcba555bb2fa5706143fdea55a
schroust <lobbies.tools5c@icloud.com>
2026-09-06 03:45:16 -0700

    Update post.js

--- src/views/post.js
+++ src/views/post.js
@@ -638,7 +638,7 @@ export function getPostHTML(post, settings, requestUrl) {
   })();
   </script>
   <script>
-  (function(){ try { fetch('/api/visit',{method:'POST',keepalive:true}).catch(function(){}); } catch(e){} })();
+  (function(){ try { if (localStorage.getItem('owner_skip')) return; fetch('/api/visit',{method:'POST',keepalive:true}).catch(function(){}); } catch(e){} })();
   </script>
 </body>
 </html>`;
